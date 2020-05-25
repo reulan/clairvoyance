4fcd208d7b264ab4762e88b0a97c3d3184545bf8
Michael Simo <mpmsimo@gmail.com>
2020-05-24 18:58:08 -0700

    moved to Terraform Exec Terraform config structure

--- app/terraform/cli.go
+++ app/terraform/cli.go
@@ -4,49 +4,53 @@ import (
 	"fmt"
 	"os"
 
-	tfexec "github.com/kmoe/terraform-exec"
+	"github.com/kmoe/terraform-exec/tfexec"
 	tfjson "github.com/hashicorp/terraform-json"
 
 	"clairvoyance/log"
 )
 
-func Configure() tfexec.Config {
-	workingDir := os.Getenv("GOPATH") + "/src/github.com/kmoe/terraform-exec/testdata"
-	cfg := tfexec.Config{
-		WorkingDir: workingDir,
-	}
-	log.Info("Clarivoyance - Created tfexec configuration")
-	return cfg
+
+func Configure(ExecPath string, WorkingDir string) tfexec.Terraform{} {
+	// Override parameters for testing
+	ExecPath := ""
+	WorkingDir := os.Getenv("GOPATH") + "/src/github.com/kmoe/terraform-exec/testdata"
+
+	// Generate a new tfexec Terraform configuration
+	tfcfg, err := tfexec.NewTerraform(ExecPath, WorkingDir)
+
+	log.Info("Clarivoyance - Created tfexec configuration.")
+	return tfcfg
 }
 
-func Init(cfg tfexec.Config) {
+func Init(tfcfg tfexec.Terraform) {
 	// Run `terraform init` so that the working directories state can be initialized.
 	log.Info("Clarivoyance - terraform init")
-	err := cfg.Init()
+	err := tfcfg.Init()
 	if err != nil {
 		panic(err)
 	}
 }
 
-func Show(cfg tfexec.Config) *tfjson.State {
+func Show(tfcfg tfexec.Terraform) *tfjson.State {
 	// Run `terraform show` against the state defined in the working directory.
 	log.Info("Clarivoyance - terraform show")
-	state, err := cfg.Show()
+	state, err := tfcfg.Show()
 	if err != nil {
 		panic(err)
 	}
 
 	// Print all returned values from the `terraform show` command (of type *tfjson.State)
-	fmt.Println(state.FormatVersion) // "0.1"
+	fmt.Println(state.FormatVersion)
 	fmt.Println(state.TerraformVersion)
 	fmt.Println(state.Values)
 
 	return state
 }
 
-func Plan(cfg tfexec.Config) *tfjson.State {
+func Plan(tfcfg tfexec.Terraform) *tfjson.State {
 	// Run `terraform plan` against the state defined in the working directory.
-	log.Info("Clarivoyance - terraform plan")
+	log.Info("Clarivoyance - terraform plan on {%s}", tfcfg.)
 	state, err := cfg.Plan()
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/hashicorp/terraform-json v0.5.0
-	github.com/kmoe/terraform-exec v0.0.0-20200509140023-684d243690a7
+	github.com/kmoe/terraform-exec v0.0.0-20200524214613-177a6c387065 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0
