4b9c546e10d09044cf19bfa56c546212aed352bb
Michael Simo <mpmsimo@gmail.com>
2020-09-02 22:21:35 -0700

    general: parse atlantis.yaml and get versions

---
+++ app/general/atlantis.go
@@ -0,0 +1,8 @@
+package general
+
+// import "gopkg.in/yaml.v2"
+
+//Parses atlantis.yaml to figure out which Terraform service to use.
+// Load tftest/atlantis.yaml
+// search key: projects:dir:terraform_version
+// return terraform_version
