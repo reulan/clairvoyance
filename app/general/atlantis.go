0cb7b299c43f3dad6b08c562f0a93fa7b03adae4
Michael Simo <mpmsimo@gmail.com>
2020-09-02 22:26:10 -0700

    update plan of action

--- app/general/atlantis.go
+++ app/general/atlantis.go
@@ -2,7 +2,9 @@ package general
 
 // import "gopkg.in/yaml.v2"
 
-//Parses atlantis.yaml to figure out which Terraform service to use.
-// Load tftest/atlantis.yaml
-// search key: projects:dir:terraform_version
-// return terraform_version
+// https://github.com/runatlantis/atlantis/tree/master/server/events/yaml
+
+// Parses atlantis.yaml to figure out which Terraform service to use.
+// By default loads tftest/atlantis.yaml or CLAIRVOYANCE_ATLANTIS_FILE if specified.
+// search in projects, get dir/terraform_version
+// return service_name, terraform_version
