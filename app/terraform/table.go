7f70c180ddd883b653f6f74935586f5cb9549949
Michael Simo <mpmsimo@gmail.com>
2021-01-05 21:08:59 -0800

    update table output (no changes / drift dectected in diff tables)

--- app/terraform/table.go
+++ app/terraform/table.go
@@ -1,6 +1,7 @@
 package terraform
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/fatih/color"
@@ -11,15 +12,39 @@ import (
 
 // this is meant for stdout to allow for easier text manipluation
 func CreateTableStdout(tsArray []*TerraformService) {
+	var ddt int = 0
+	var nct int = 0
+
 	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
 	columnFmt := color.New(color.FgYellow).SprintfFunc()
 
-	tbl := table.New("Project Name", "Version", "Add", "Change", "Delete", "Information")
-	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	driftDetectedTable := table.New("Project Name", "Version", "Add", "Change", "Delete", "Information")
+	driftDetectedTable.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+	noChangesTable := table.New("Project Name", "Version", "Information")
+	noChangesTable.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 	for _, service := range tsArray {
-		tbl.AddRow(service.ProjectName, service.TerraformVersion, strconv.Itoa(service.CountAdd), strconv.Itoa(service.CountChange), strconv.Itoa(service.CountDestroy), service.Summary)
+		if service.Summary == "No Changes." {
+			noChangesTable.AddRow(service.ProjectName, service.TerraformVersion, service.Summary)
+			log.Debug("[CreateTableStdout] Terraform service contains no drift.")
+			nct++
+		} else {
+			driftDetectedTable.AddRow(service.ProjectName, service.TerraformVersion, strconv.Itoa(service.CountAdd), strconv.Itoa(service.CountChange), strconv.Itoa(service.CountDestroy), service.Summary)
+			log.Debug("[CreateTableStdout] Added %s to driftDetectedTable.", service.ProjectName)
+			ddt++
+		}
+
 	}
 
-	tbl.Print()
-	log.Debug("Sent Drift Report table to stdout.")
+	// Find a better way omit tables
+	fmt.Println("")
+	if ddt >= 1 {
+		driftDetectedTable.Print()
+		fmt.Println("")
+	}
+	if nct >= 1 {
+		noChangesTable.Print()
+		fmt.Println("")
+	}
+	log.Debug("Sent Drift Report tables to stdout.")
 }
